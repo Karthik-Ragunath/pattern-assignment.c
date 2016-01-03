8d5bf16aa01b1ab4347645d7935c54fdbcdf5de0
Karthik-Ragunath <a.karthikragunath@gmail.com>
2016-01-04 02:34:32 +0530

    Update pattern-2.c

--- pattern-2.c
+++ pattern-2.c
@@ -2,42 +2,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 int main(void) {
 	int rows,i,j,number;
 	int value=1;
 	printf("enter the number of rows :\n");
 	scanf("%d",&rows);
 	int *arr[rows];
-	for(i=0;i<rows;i++)
+	for(i=0;i<(rows+1);i++)
 	{
 		arr[i]=(int*)malloc(rows*sizeof(int));
 	}
-	for(i=0;i<rows;i++)
+	for(i=0;i<=rows;i++)
 	{
 		for(j=0;j<=rows;j++)
 		{
 			arr[i][j]=0;
 		}
-	}
-	if(rows<3)
-	{
-		for(i=0;i<rows;i++)
-		{
-			for(j=0;j<=i;j++)
-			{
-				arr[i][j]=value+(2*j);
-			}
-			value++;
-		}
-		for(i=0;i<rows;i++)
-		{
-			for(j=0;j<=i;j++)
-			{
-				printf("%d ",arr[i][j]);
-			}
-			printf("\n");
-		}
-		return 0;
 	}
 	for(i=0;i<2;i++)
 	{
